4be70022ca9a01126b77db780169b74163b404a1
nima-hashi <nimaahashii@gmail.com>
2024-06-11 19:34:00 -0400

    Time: 77 ms (40.21%), Space: 21.3 MB (5.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        # make hashmap for every str
+        # compare and add to list
+
+        def makeHash(s):
+            hm = {}
+            for i in s:
+                if i in hm:
+                    hm[i] += 1
+                else:
+                    hm[i] = 1
+            return tuple(sorted(hm.items()))
+
+        hm = {}
+        for s in strs:
+            key = makeHash(s)
+
+            if key in hm:
+                hm[key].append(s)
+            else:
+                hm[key] = [s]
+
+        return list(hm.values())
+
+
+    
+
+
+        
