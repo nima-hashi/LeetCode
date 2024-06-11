5724c44ceb6583140d97a90d853013546c7b8420
nima-hashi <nimaahashii@gmail.com>
2024-06-11 18:18:06 -0400

    Time: 4 ms (99.51%), Space: 12 MB (5.32%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:
-            return ""
-    
-   
-        for i, char in enumerate(strs[0]):
-      
-            for string in strs[1:]:
-                if i >= len(string) or string[i] != char:
-
-                    return strs[0][:i]
-
-        return strs[0]
+        res = ""
         
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+                
+        
+       
