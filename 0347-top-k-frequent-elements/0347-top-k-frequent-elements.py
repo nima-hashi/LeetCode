6027617b696038dc51c043821310285d69fe300f
nima-hashi <nimaahashii@gmail.com>
2024-07-02 17:07:29 -0400

    Time: 70 ms (78.70%), Space: 15 MB (55.08%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+    
+        # Step 1: Count the frequencies of each number
+        counter = Counter(nums)
+        
+        # Step 2: Group numbers by their frequencies
+        cntToVal = defaultdict(list)
+        for val, cnt in counter.items():
+            cntToVal[cnt].append(val)
+        
+        # Step 3: Extract the top k frequent elements
+        res = []
+        # Get the unique counts and sort them in descending order
+        cnts = sorted(cntToVal.keys(), reverse=True)
+        
+        # Iterate through the counts and collect the top k elements
+        for cnt in cnts:
+            for num in cntToVal[cnt]:
+                if len(res) < k:
+                    res.append(num)
+                else:
+                    return res
+        
+        return res
+        
+            
+        
+
+        
+
+       
