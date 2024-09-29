37a21dd819d417cbb3c025ff338c4a3dede10668
nima-hashi <nimaahashii@gmail.com>
2024-09-29 14:03:56 -0400

    Time: 132 ms (37.86%), Space: 41.4 MB (85.32%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -5,31 +5,83 @@
 #         self.next = None
 
 class Solution(object):
+    def getLen(self, node):
+        
+        c = 0
+        curr = node
+        while curr:
+            c += 1
+            curr = curr.next
+        return c
+    
+    def findStartOfPath(self, node, move):
+        
+        curr = node
+        while curr and move != 0:
+            curr = curr.next
+            move -= 1
+        
+        return curr
+    
+    def intersection(self, startA, startB):
+        
+        while startA and startB:
+            if startA == startB:
+                return startA
+            startA = startA.next
+            startB = startB.next
+        return None    
+        
     def getIntersectionNode(self, headA, headB):
         """
         :type head1, head1: ListNode
         :rtype: ListNode
         """
         
-        cache = {}
+        c1 = self.getLen(headA)
+        c2 = self.getLen(headB)
         
-        curr = headA
+        pathLen = min(c1, c2)
         
-        while curr:
-            if curr not in cache:
-                cache[curr] = 1
-                curr = curr.next
+        if c1 > pathLen:
+            move = c1 - pathLen
+            startA = self.findStartOfPath(headA, move)
+        
+        else:
+            startA = headA
+            
+        if c2 > pathLen:
+            move = c2 - pathLen
+            startB = self.findStartOfPath(headB, move)
+            
+        else:
+            startB = headB
+        
+        return self.intersection(startA, startB)
+        
+        
+        
+        
+        
+#         cache = {}
+        
+#         curr = headA
+        
+#         while curr:
+#             if curr not in cache:
+#                 cache[curr] = 1
+#                 curr = curr.next
                 
-        curr = headB
+#         curr = headB
         
-        while curr:
-            if curr not in cache:
-                cache[curr] = 1
-                curr = curr.next
+#         while curr:
+#             if curr not in cache:
+#                 cache[curr] = 1
+#                 curr = curr.next
             
-            else: 
-                return curr
+#             else: 
+#                 return curr
             
-        return None
+#         return None
         
         
